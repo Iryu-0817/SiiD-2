b0fc307af070984f918529f6d9270cdb000ea7ef
Iryu-0817 <iryu20030817@gmail.com>
2024-04-24 13:08:11 +0900

    Did the "git commit";

--- w11/Laravel-pokeAPI/resources/views/pokemon_info.blade.php
+++ w11/Laravel-pokeAPI/resources/views/pokemon_info.blade.php
@@ -6,9 +6,9 @@
     <title>Pokemon Info</title>
 </head>
 <body>
-    <h1>Pokemon Info</h1>
+    <h1>Pokemon Info</h1> <p></p>
 @if(isset($pokemon))
-    <h2>{{ strtoupper($pokemon['name']) }}</h2>
+    <h2>{{ $pokemon['id'] }}. {{ strtoupper($pokemon['name']) }}</h2>
     <img src="{{ $pokemon['sprites']['front_default'] }}" alt="{{ $pokemon['name'] }}">
     <p><strong>Height:</strong> {{ $pokemon['height'] / 10 }} m</p>
     <p><strong>Weight:</strong> {{ $pokemon['weight'] / 10 }} kg</p>
